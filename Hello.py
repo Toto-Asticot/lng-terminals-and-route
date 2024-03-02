1a896ff96baef9d35f579352ead438229c34d377
Toto-Asticot <150086754+Toto-Asticot@users.noreply.github.com>
2024-03-02 23:23:35 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -66,7 +66,7 @@ def run():
     st.markdown("<h1 style='text-align: center;'><i class='fas fa-ship'></i> Terminal Route Visualization <i class='fas fa-ship'></i></h1>", unsafe_allow_html=True)
     # User input for start and end terminals
     start_terminal = st.selectbox("Select Start Terminal:", options=terminal_df[terminal_df["FacilityType"] == "Export"]["TerminalName"].tolist(), index=9)
-    end_terminal = st.selectbox("Select End Terminal:", options=terminal_df[terminal_df["FacilityType"] == "Import"]["TerminalName"].tolist(), default="Le Havre FSRU")
+    end_terminal = st.selectbox("Select End Terminal:", options=terminal_df[terminal_df["FacilityType"] == "Import"]["TerminalName"].tolist(), index=terminal_df[terminal_df["FacilityType"] == "Import"]["TerminalName"].tolist().index("Le Havre FSRU"))
     
     passage=["babalmandab","bosporus","gibraltar","suez","panama","ormuz"]
     selected_values = st.multiselect('Select unaccessible routes:', passage)+["northwest"]
