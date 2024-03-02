52de8ac68cc7ca57a846062d592c15dca837b9fb
Toto-Asticot <150086754+Toto-Asticot@users.noreply.github.com>
2024-03-02 13:46:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_streamlit_app',
+    name='lng-terminals-and-route',
     version='0.1',
     install_requires=[
             "altair",
